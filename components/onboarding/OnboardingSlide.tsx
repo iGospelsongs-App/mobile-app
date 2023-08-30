48d93b2505552843c198af205b4b4647e58b08b1
omatsola619 <omarsunday0@gmail.com>
2023-08-30 12:14:00 +0100

    completed onboarding slide screens

---
+++ components/onboarding/OnboardingSlide.tsx
@@ -0,0 +1,139 @@
+import { Dimensions, ImageBackground, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React from 'react'
+import { LinearGradient } from 'expo-linear-gradient'
+
+const {width, height} = Dimensions.get('window');
+
+const OnboardingSlide = ({item, data, slideIndex}: any) => {
+  return (
+    <View style={styles.container}>
+          <ImageBackground source={item.image} resizeMode='cover' style={styles.myImg}>
+              <LinearGradient
+                  colors={['transparent', 'rgba(0,0,0,0.8)', 'rgba(0, 0, 0, 0.972)', 'black']}
+                  style={styles.grad}
+              />
+              <View style={styles.contentWrapper}>
+                <View style={styles.line}></View>
+                  <View style={styles.textWrapper}>
+                      <Text style={styles.title}>
+                          {item.title}
+                      </Text>
+                  </View>
+
+                  {/* if the button is true, show it here  */}
+                  {
+                      item.btn && (
+                          <>
+                              <TouchableOpacity activeOpacity={0.8} style={styles.btnWrapper}>
+                                  <Text style={styles.btnText}>Get Started</Text>
+                              </TouchableOpacity>
+                              <Text style={styles.loginLink}>
+                                Have an account? Log in
+                              </Text>
+                          </>
+
+                      )
+                  }
+
+                  {/* if the indicator is true  */}
+                  {
+                      item.indicator && (
+                          <View style={styles.indicatorWrapper}>
+                              {data.map((_: any, index: number) => (
+                                  <View
+                                      key={index}
+                                      style={[
+                                          styles.indicator,
+                                          slideIndex === index && {
+                                              backgroundColor: "white",
+                                              width: 13,
+                                              height: 13,
+                                          },
+                                      ]}
+                                  />
+                              ))}
+                          </View>
+                      )
+                  }
+
+               
+              </View>
+          </ImageBackground>
+          
+    </View>
+  )
+}
+
+export default OnboardingSlide
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    myImg: {
+        height: '100%',
+        width,
+        flex: 1
+    },
+    grad: {
+        position: 'absolute',
+        left: 0,
+        right: 0,
+        bottom: 0,
+        height: 350,
+    },
+    contentWrapper: {
+        flex: 1,
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        marginBottom: 50
+    },
+    line: {
+        width: 60,
+        height: 5,
+        backgroundColor: '#cf0608',
+    },
+    title: {
+       color: 'white',
+       marginTop: 20,
+       textAlign: 'center',
+       fontFamily: 'sf-med',
+       fontSize: 25,
+       height: 100
+    },
+    textWrapper: {
+        width: '60%'
+    },
+    btnWrapper: {
+        backgroundColor: 'white',
+        marginTop: 18,
+        borderRadius: 6
+    },
+    btnText: {
+        paddingHorizontal: 42,
+        paddingVertical: 12,
+        fontFamily: 'sf-bold',
+        fontSize: 16,
+    },
+    loginLink: {
+        fontSize: 16,
+        fontFamily: 'sf-reg',
+        color: 'white',
+        textAlign: 'center',
+        marginTop: 25
+    },
+    indicator: {
+        height: 13,
+        width: 13,
+        backgroundColor: 'grey',
+        marginHorizontal: 3,
+        borderRadius: 50,
+      },
+      indicatorWrapper: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginTop: 70,
+        marginBottom: 30
+      }
+})
--- screens/onboarding/Onboarding.tsx
+++ screens/onboarding/Onboarding.tsx
@@ -1,12 +1,83 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Dimensions, Image, StyleSheet, Text, ImageBackground, FlatList } from 'react-native'
+import React, { useState } from 'react'
+import img1 from '../../assets/images/ob1.png'
+import img2 from '../../assets/images/ob2.png'
+import img3 from '../../assets/images/ob3.png'
+import PagerView from 'react-native-pager-view';
+import { LinearGradient } from 'expo-linear-gradient';
+import OnboardingSlide from '../../components/onboarding/OnboardingSlide'
+
+const {width, height} = Dimensions.get('window');
+
+interface OnboardingDataType {
+    id: number,
+    image: string,
+    title: string,
+    btn: boolean,
+    indicator: boolean
+}
+
+const onBoardingData: OnboardingDataType[] = [
+    {
+        id: 1,
+        image: img1,
+        title: 'Awaken your spirit with Heavenly melodies',
+        btn: false,
+        indicator: true
+    },
+    {
+        id: 2,
+        image: img3,
+        title: 'Elevate your soul with praise and worship',
+        btn: false,
+        indicator: true
+    },
+    {
+        id: 3,
+        image: img2,
+        title: 'Worship in truth and in spirit',
+        btn: true,
+        indicator: false
+    },
+]
+
 
 const Onboarding = () => {
+    const [currentSlideIndex, setCurrentSlideIndex] = useState(0)
+
+    const updateCurrentSlideIndex = (e: any) => {
+      const contentOffsetX = e.nativeEvent.contentOffset.x;
+      const currentIndex = Math.round(contentOffsetX / width);
+      setCurrentSlideIndex(currentIndex)
+    }
+
   return (
-    <View>
-      <Text>Onboarding</Text>
+    <View style={styles.container}>
+          
+          {/* <Text style={{
+            position: 'absolute',
+            top: '20%',
+            zIndex: 10
+          }}>This is me</Text> */}
+          <FlatList
+              onMomentumScrollEnd={updateCurrentSlideIndex}
+              data={onBoardingData}
+              contentContainerStyle={{ height: '100%' }}
+              showsHorizontalScrollIndicator={false}
+              horizontal
+              renderItem={({ item }) => <OnboardingSlide data={onBoardingData} slideIndex={currentSlideIndex} item={item} />}
+              pagingEnabled
+              bounces={false}
+          />
+      
     </View>
   )
 }
 
 export default Onboarding
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+})
