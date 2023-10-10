57073ffc7ea6fc1a5f2f3607aaae6151f074d94e
omatsola619 <omarsunday0@gmail.com>
2023-10-10 17:21:25 +0100

    started fixing home screen

--- screens/authenticated/Home.tsx
+++ screens/authenticated/Home.tsx
@@ -1,4 +1,4 @@
-import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { Platform, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import { StatusBar } from 'expo-status-bar'
@@ -12,34 +12,58 @@ import frame2 from '../../assets/images/frame2.png'
 import frame3 from '../../assets/images/frame3.png'
 import frame4 from '../../assets/images/frame4.png'
 
+interface DataProp {
+  title: string,
+  image: any,
+}
+
+const data: DataProp[] = [
+  {
+    title: 'Journey to the deep',
+    image: frame1
+  },
+  {
+    title: 'Walk by faith',
+    image: frame2
+  },
+  {
+    title: 'The birth of revival',
+    image: frame4
+  },
+  {
+    title: 'Hallelujah',
+    image: frame3
+  },
+]
+
 const Home = () => {
 
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <StatusBar style="light" />
+
+      {/* header section  */}
+      <SafeAreaView style={styles.header}>
+        {/* left section */}
+        <View style={styles.headerLeft}>
+          <View style={styles.imageCont}>
+            <Image source={dp} style={styles.dp} />
+          </View>
+          <View style={styles.nameCont}>
+            <Text style={styles.greet}>Good Morning</Text>
+            <Text style={styles.name}>Omatsola Sunday</Text>
+          </View>
+        </View>
+        {/* right section  */}
+        <View style={styles.headerRight}>
+          <Image source={bell} />
+          <Image source={settings} style={styles.sett} />
+        </View>
+      </SafeAreaView>
+
       <ScrollView style={styles.wrapper} showsHorizontalScrollIndicator={false}>
         {/* contents  */}
         <View style={styles.content}>
-
-          {/* header section  */}
-          <View style={styles.header}>
-            {/* left section */}
-            <View style={styles.headerLeft}>
-              <View style={styles.imageCont}>
-                <Image source={dp} style={styles.dp} />
-              </View>
-              <View style={styles.nameCont}>
-                <Text style={styles.greet}>Good Morning</Text>
-                <Text style={styles.name}>Omatsola Sunday</Text>
-              </View>
-            </View>
-            {/* right section  */}
-            <View style={styles.headerRight}>
-              <Image source={bell} />
-              <Image source={settings} style={styles.sett} />
-            </View>
-          </View>
-
           {/* slide section  */}
           <View>
             <View style={styles.slideSection}>
@@ -53,23 +77,42 @@ const Home = () => {
           {/* items  */}
           <View style={styles.itemsSection}>
             {/* top songs section  */}
-            <View>
+            <View style={{marginBottom: 67}}>
               <SectionHeader title="Top songs in Nigeria" color='#30A712' />
-              <View>
-                <View style={styles.cardCover}>
-                  <Image source={frame1} style={styles.cardImg} />
-                  <Text style={styles.cardDesc}>Journey to the deep</Text>
-                </View>
-
-              </View>
+              <ScrollView horizontal style={styles.itemList}>
+                {
+                  data.map((item, i) => (
+                    <TouchableOpacity activeOpacity={0.8} key={i} style={styles.cardCover}>
+                      <Image source={item.image} style={styles.cardImg} />
+                      <Text style={styles.cardDesc}>{item.title}</Text>
+                    </TouchableOpacity>
+                  ))
+                }
+              </ScrollView>
             </View>
+
+            {/* praise melodies section */}
+            <View>
+              <SectionHeader title="Praise melodies" color='#E31b54' />
+              <ScrollView horizontal style={styles.itemList}>
+                {
+                  data.map((item, i) => (
+                    <TouchableOpacity activeOpacity={0.8} key={i} style={styles.cardCover}>
+                      <Image source={item.image} style={styles.cardImg} />
+                      <Text style={styles.cardDesc}>{item.title}</Text>
+                    </TouchableOpacity>
+                  )).reverse()
+                }
+              </ScrollView>
+            </View>
+
           </View>
           
         </View>
 
       </ScrollView>
       
-    </SafeAreaView>
+    </View>
   )
 }
 
@@ -78,15 +121,16 @@ export default Home
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'black',
-    flex: 1
+    flex: 1,
+    borderBottomWidth: 0,
   },
   wrapper: {
-    flex: 1
+    flex: 1,
   },
   content: {
     flex: 1,
-    marginTop: 18,
-    paddingHorizontal: 16
+    paddingHorizontal: 16,
+    paddingBottom: 25
   },
   dp: {
     width: 35,
@@ -112,7 +156,10 @@ const styles = StyleSheet.create({
   header: {
       flexDirection: 'row',
       alignItems: 'center',
-      justifyContent: 'space-between'
+      justifyContent: 'space-between',
+      marginTop: 18,
+      paddingHorizontal: 16,
+      marginBottom: Platform.OS === 'android' ? 20 : -10,
   },
   headerLeft: {
       flexDirection: "row",
@@ -124,7 +171,7 @@ const styles = StyleSheet.create({
       marginLeft: 16
   },
   slideSection: {
-    marginTop: 44,
+    marginTop: 24,
     width: '100%',
     alignItems: 'center',
     height: 168
@@ -134,6 +181,7 @@ const styles = StyleSheet.create({
   },
   cardCover: {
     width: 137,
+    marginRight: 16
   },
   cardImg: {
     width: 137, 
@@ -144,6 +192,9 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontFamily: 'sf-med',
     marginTop: 12
+  },
+  itemList: {
+    flexDirection: 'row',
   }
 
 
